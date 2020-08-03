028162ff198c4885f9f0a00f52e2a41c4a1b3ea2
Yacine Serir <yserir@egencia.com>
2020-08-03 11:07:37 +0200

    start adding roadmap builder component and libs

---
+++ ng-app/src/app/roadmap-builder/roadmap-builder.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap-builder',
+  templateUrl: './roadmap-builder.component.html',
+  styleUrls: ['./roadmap-builder.component.css']
+})
+export class RoadmapBuilderComponent implements OnInit {
+
+  height : number;
+  width : number;
+  
+
+  constructor() {
+
+  }
+
+  
+
+  ngOnInit(): void {
+  }
+
+}
